fbeb80e51297340990c7059cf8a01ec382fae8f7
Gaston <gaston@mac.local>
2015-11-22 21:56:33 -0300

    first poc

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-'use strict';
-
-const Glue = require('glue');
-const Manifest = require('./manifest');
-
-
-const composeOptions = {
-    relativeTo: __dirname
-};
-
-
-module.exports = Glue.compose.bind(Glue, Manifest.get('/'), composeOptions);
+module.exports = require('./lib');
